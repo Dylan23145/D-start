5df36ba2b20e1ba891bc246891fde6ffc9c89c79
Dylan23145 <74885040+Dylan23145@users.noreply.github.com>
2021-01-28 23:52:18 -0500

    Create D-start.sh
    Este repositorio es para facilitar a los que están entrando xd o años que se les ha borrado todo...

---
+++ D-start.sh
@@ -0,0 +1,143 @@
+pkg install python
+
+
+pkg inatall python2
+
+
+pkg install git
+
+pkg install php
+
+pkg install nmap
+
+pkg install curl
+
+pkg install lolcat
+
+pkg install axel
+
+
+git clone https://github.com/DeepSociety/wiki-termux
+
+git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+
+git clone https://github.com/Anonymous-Zpt/T-root
+
+git clone https://github.com/abhinavkavuri/PhoneInfoga
+
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+
+git clone git clone https://github.com/maldevel/IPGeoLocation
+
+clear
+
+cuenta=10
+while [ $cuenta != 0 ]; do
+        echo borrando archivos en:: $cuenta
+        sleep 0.5
+        let cuenta=cuenta-1
+done
+
+
+gris='\033[1;30m'
+rojo='\033[0;31m'
+rojoclaro='\033[1;31m'
+verde='\033[1;32m'
+amarillo='\033[1;33m'
+azul='\033[0;34m'                                                             lila='\033[0;35m'
+lilaclaro='\033[1;35m'
+cian='\033[0;36m'
+blanco='\033[1;37m'
+DEFAULT='\033[0m'
+
+colores=($gris $rojo $rojoclaro $verde $amarillo $azul $lila $lilaclaro $cian
+$blanco $DEFAULT )
+for c in "${colores[@]}";do
+    printf "\r $c Te la cresite jsjs  $DEFAULT "
+    sleep 0.2
+done
+
+
+
+
+
+clear
+
+termux-open https://chat.whatsapp.com/Fnn2Bqt67TpAGvMrxTOjaz
+
+
+
+echo " NO OLVIDES USAR : cat README.md para ver como funcionan"
+
+echo " esto es para instalar las herramientas basicas
+creditos a sus respectivos creadotes "
+
+
+
+
+
+
+
+
+
+
+
+echo " HERRAMIENTAS
+
+
+Wiki-Termux: esta herramienta es muy buena si estas comenzando ya que tiene mucha variedad de herramientas para phishing ataques ddos etc..
+
+
+FotoSploit: esta herramienta es par phishing es muy buena
+
+
+T-root: como su nombre  lo dice es lara hacerse root sin tener root
+
+
+PhoneInfoga: esta herramienta sirve para ver informacion de numeros
+en algunos paises puedes ver la localizacion de donde se copro el numero
+
+
+Termux-ohmyzsh: simplemente para personalizar tu termux y que quede re lindo :p
+
+IPGeoLocation: es lara ver la ubicsciln exacta de una ip"
+
+
+echo " RECUERDA COMPARTIR "
+
+                                                                                                                                                                                 
+
+gris='\033[1;30m'
+rojo='\033[0;31m'
+rojoclaro='\033[1;31m'
+verde='\033[1;32m'
+amarillo='\033[1;33m'
+azul='\033[0;34m'
+lila='\033[0;35m'
+lilaclaro='\033[1;35m'
+cian='\033[0;36m'
+blanco='\033[1;37m'
+DEFAULT='\033[0m'
+
+colores=($gris $rojo $rojoclaro $verde $amarillo $azul $lila $lilaclaro $cian $blanco $DEFAULT )
+for c in "${colores[@]}";do
+    printf "\r $c porfavor comoartan :] $DEFAULT "
+    sleep 0.3
+done
+
+
+gris='\033[1;30m'
+rojo='\033[0;31m'                                                             rojoclaro='\033[1;31m'
+verde='\033[1;32m'
+amarillo='\033[1;33m'
+azul='\033[0;34m'                                                             lila='\033[0;35m'                                                             lilaclaro='\033[1;35m
+'                                                        cian='\033[0;36m'
+blanco='\033[1;37m'
+DEFAULT='\033[0m'
+
+colores=($gris $rojo $rojoclaro $verde $amarillo $azul $lila $lilaclaro $cian
+$blanco $DEFAULT )
+for c in "${colores[@]}";do
+    printf "\r $c ECHO POR: Dylan xd  $DEFAULT "
+    sleep 1
+done
