4141f51d11ac09e39b4e61250a9e2214f8c73d8e
Dylan23145 <74885040+Dylan23145@users.noreply.github.com>
2021-01-28 21:02:50 -0500

    D-start
    Un simple repositorio por si borras termux y todos sus archivos o vienes a empezar xd
    
    Se ejecuta asi:
    
    bash D-start
    
    Y listo xd

---
+++ D-start.sh
@@ -0,0 +1,98 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "乃ㄚ ᴅᴛʟᴀɴ"
+
+
+echo "
+
+
+
+░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░
+
+ ░█░░░░░░░░▀▄░░░░░░▄░
+
+  █░░▀░░▀░░░░░▀▄▄░░█░█
+
+   █░▄░█▀░▄░░░░░░░▀▀░░█
+
+    █░░▀▀▀▀░░░░░░░░░░░░█
+
+     █░░░░░░░░░░░░░░░░░░█
+
+      █░░░░░░░░░░░░░░░░░░█
+
+       ░█░░▄▄░░▄▄▄▄░░▄▄░░█░
+
+        ░█░▄▀█░▄▀░░█░▄▀█░▄▀░
+
+         ░░▀░░░▀░░░░░▀░░░▀░░░
+
+         "
+
+
+pkg install python
+
+
+pkg inatall python2
+
+pkg install -y python3
+
+pkg install git
+
+pkg install php
+
+pkg install php2
+
+pkg install nmap
+
+pkg install curl
+
+pkg install locat
+
+
+git clone https://github.com/DeepSociety/wiki-termux
+
+git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+
+git clone https://github.com/Anonymous-Zpt/T-root
+
+git clone https://github.com/abhinavkavuri/PhoneInfoga
+
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+
+clear
+
+
+termux-open https://chat.whatsapp.com/Fnn2Bqt67TpAGvMrxTOjaz
+
+
+
+echo " NO OLVIDES USAR : cat README.md para ver como funcionan"
+
+echo " esto es para instalar las herramientas basicas
+creditos a sus respectivos creadotes "
+
+
+
+
+
+
+echo " HERRAMIENTAS
+
+
+Wiki-Termux: esta herramienta es muy buena si estas comenzando ya que tiene mucha variedad de herramientas para phishing ataques ddos etc..
+
+
+FotoSploit: esta herramienta es par phishing es muy buena
+
+
+T-root: como su nomnre lo dice es lara hacerse root sin temer root
+
+
+PhoneInfoga: esta herramienta sirve oara ver informacion de numeros
+en algunos paises puedes ver la localizacion de donde se comoro el numero
+
+
+Termux-ohmyzsh: simolemente para personalizar tu termux y que quede re lindo :p"
+
+
